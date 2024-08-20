517c3c035f0e958918db6cc9bb2f05e81e9a797e
Daniela Jardim <danialvesdesigner@gmail.com>
2024-08-20 19:02:08 -0300

    add global style

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,10 +1,22 @@
 import * as React from "react"
 
+import styled from "styled-components"
+import GlobalStyles from "../../styles/global"
+
+const LayoutWrapper = styled.section `
+  display: flex;
+  background: #16202c;
+  min-height: 100vh;
+  width:100%
+`
+
+
 const Layout = ({ children }) => {
   return (
-    <>
+    <LayoutWrapper>
+    <GlobalStyles/>
       <main>{children}</main>
-    </>
+    </LayoutWrapper>
   )
 }
 
